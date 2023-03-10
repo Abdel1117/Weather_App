b06d97442bf89cdbe6f7f8a7b5ce5255b2dc83ab
Abderahmane Adjali <abderahmane.adjali@live.fr>
2023-03-10 11:55:43 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import ForeCast from './components/ForeCast/ForeCast'
+import Nav from './components/Nav/Nav'
 import Home from './pages/Home/Home'
 function App() {
 
   return (
+    <>
+    <Nav />
     <main className="App">
       <Home />
       <section className='Banner_foreCast'>
@@ -17,6 +20,7 @@ function App() {
         <ForeCast day={7} />
       </section>
     </main>
+    </>
   )
 }
 
